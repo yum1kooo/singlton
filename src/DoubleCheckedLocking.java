677e4dd21d8bbce8201e8ce12866b0db9060d608
Kirill <kirillkhromov@mail.ru>
2025-11-17 00:36:07 +0300

    add double check

--- src/DoubleCheckedLocking.java
+++ src/DoubleCheckedLocking.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class DoubleCheckedLocking {
+    public static void main(String[] args) {
+            Dab dab = Dab.getInstance();
+
+    }
+}
+
+class Dab{
+    private static Dab INSTANCE;
+
+    private Dab(){}
+
+    public static Dab getInstance(){
+        if(INSTANCE == null){
+            synchronized (Dab.class) {
+                if(INSTANCE == null) {
+                    INSTANCE = new Dab();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 }
