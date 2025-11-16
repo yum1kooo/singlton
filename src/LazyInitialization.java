49d09d3ec9e89b1a8aa3ceab7abb6c56c9530697
Kirill <kirillkhromov@mail.ru>
2025-11-17 00:20:29 +0300

    add simple solution

--- src/LazyInitialization.java
+++ src/LazyInitialization.java
@@ -1,5 +1,19 @@
 public class LazyInitialization {
     public static void main(String[] args) {
+        Singlton singlton = Singlton.getInstance();
+    }
+}
 
+class Singlton {
+    private static Singlton INSTANCE;
+
+    private Singlton() {
+    }
+
+    public static Singlton getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new Singlton();
+        }
+        return INSTANCE;
     }
 }
